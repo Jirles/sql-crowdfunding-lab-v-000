80ae0369dae56a2d15e2407e67c4fe24a07d6565
Emily Jirles <emily.jirles@gmail.com>
2018-05-14 19:45:45 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -15,7 +15,7 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"SELECT pr.title, pl.amount - pr.funding_goal AS amount_over_goal FROM projects pr INNER JOIN pledges pl ON pr.id = pl.project_id GROUP BY pr.title HAVING pl.amount > pr.funding_goal;"
+"SELECT pr.title, pl.amount - pr.funding_goal AS amount_over_goal FROM projects pr INNER JOIN pledges pl ON pr.id = pl.project_id HAVING pl.amount > pr.funding_goal GROUP BY pr.title;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
@@ -23,7 +23,7 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"SELECT pr.category, SUM(pl.amount) FROM projects pr INNER JOIN pledges pl ON pr.id = pl.project_id WHERE pr.category = 'music';"
+"SELECT pr.category, pl.amount FROM pledges pl LEFT JOIN projects pr ON pr.id = pl.project_id WHERE pr.category = 'music';"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
